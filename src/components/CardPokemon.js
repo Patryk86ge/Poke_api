7d69c87f7f396e8b5e62b249ab1d9a7cbd3ebd15
Patryk86ge <kokot.patryk@gmail.com>
2022-04-14 10:05:26 +0200

    change_elements_of_style

---
+++ src/components/CardPokemon.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {Button} from "react-bootstrap";
+import ModalPokemon from "../components/modalPokemon";
+
+const CardPokemon = ({pokemon}) => {
+
+  return (
+    <div className={"section__allPokeBox container"}>
+      {
+        pokemon.map((poke) => {
+          return (
+            <div
+              key={poke.id}
+              className={"section__pokeBox"}
+            >
+              <div className="section__pokeBoxImg">
+                <img src={poke.sprites.front_shiny} />
+              </div>
+              <div className="section__specification">
+                <h2>{poke.name}</h2>
+                <p>height: {poke.height}</p>
+                <p>weight: {poke.weight}</p>
+                <p>order: {poke.order}</p>
+                <ModalPokemon pokemon={poke}/>
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  );
+};
+
+export default CardPokemon;
